d4da5e7b04d39fcfbbf41b542ed25cb14adbd860
rodrigosqr <rodrigosiqueira.01@gmail.com>
2016-10-03 22:56:34 -0300

    inicio cadastro cerveja

--- brewer/src/main/java/com/rodrigo/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/rodrigo/brewer/controller/CervejasController.java
@@ -1,7 +1,15 @@
 package com.rodrigo.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.rodrigo.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
@@ -9,6 +17,16 @@ public class CervejasController {
 	@RequestMapping("/cervejas/novo")
 	public String novo(){
 		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes){
+		if(result.hasErrors()){
+			model.addAttribute("mensagem", "Erro no formulário!");
+			return "cerveja/CadastroCerveja";
+		}
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		return "redirect:/cervejas/novo";
 	}
 
 }
---
+++ brewer/src/main/java/com/rodrigo/brewer/model/Cerveja.java
@@ -0,0 +1,44 @@
+package com.rodrigo.brewer.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cerveja {
+	
+	@NotBlank
+	private String sku;
+	
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	@Size(min=1, max=10)
+	private String descricao;
+
+	public Cerveja(){}
+	
+	public Cerveja(String sku, String nome) {
+		this.sku = sku;
+		this.nome = nome;
+	}
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
